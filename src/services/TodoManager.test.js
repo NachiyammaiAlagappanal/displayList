6433ff6c395f9750183a61e9c1b22543b3082cc3
NACHIYAMMAI <nachiyammai.al@gmail.com>
2022-04-04 15:16:42 +0530

    TodoManager tests written for - filter, edit.

--- src/services/TodoManager.test.js
+++ src/services/TodoManager.test.js
@@ -4,7 +4,8 @@ import * as random from '@laufire/utils/random';
 
 describe('TodoManager', () => {
 	const { getText, getTodosCount, getActiveCount, AddTodo,
-		toggleTodo, removeTodo, toggleAll, ClearCompleted } = TodoManager;
+		toggleTodo, removeTodo, toggleAll, ClearCompleted,
+		hasActiveCount, filterTodos, hasNoTodos, editTodo } = TodoManager;
 
 	const randomStringOne = Symbol('string');
 	const randomTextOne = Symbol('text');
@@ -34,6 +35,7 @@ describe('TodoManager', () => {
 			text: newRandomText,
 			completed: false,
 		},
+
 	};
 	const existingTodos = context.state.todos;
 	const [changedTodo, unChangedTodo] = existingTodos;
@@ -60,13 +62,6 @@ describe('TodoManager', () => {
 
 		expect(result).toEqual(2);
 	});
-	// describe('filters', () => {
-	// 	test('all', () => {
-	// 		const result = filters.all();
-
-	// 		expect(result).toEqual(true);
-	// 	});
-	// });
 	test('AddTodo', () => {
 		jest.spyOn(TodoManager, 'getText').mockReturnValue(context.data);
 		const result = AddTodo(context);
@@ -102,5 +97,91 @@ describe('TodoManager', () => {
 		const result = ClearCompleted(todos);
 
 		expect(result).toEqual([unChangedTodo]);
+	});
+
+	describe('ActiveCountCheck', () => {
+		test('hasActiveCount FalseCondition', () => {
+			const result = hasActiveCount(context);
+
+			expect(result).toEqual(false);
+		});
+		test('hasActiveCount TrueCondition', () => {
+			const ModifiedContext = {
+				state: {
+					todos: [{ ...existingTodos,
+						completed: !existingTodos.completed }],
+				},
+			};
+
+			const result = hasActiveCount(ModifiedContext);
+
+			expect(result).toEqual(true);
+		});
+	});
+
+	describe('all', () => {
+		test('filteringAllTodos', () => {
+			const state = {
+				...context.state,
+				filter: 'all',
+			};
+			const result = filterTodos({ state });
+
+			expect(result).toEqual(context.state.todos);
+		});
+		test('filteringActiveTodos', () => {
+			const ModifiedContext = {
+				state: {
+					todos: [{ ...changedTodo,
+						completed: !changedTodo.completed }, unChangedTodo],
+					filter: 'active',
+				},
+			};
+			const result = filterTodos(ModifiedContext);
+
+			expect(result).toEqual([unChangedTodo]);
+		});
+		test('filteringActiveTodos', () => {
+			const ModifiedContext = {
+				state: {
+					todos: [{ ...changedTodo,
+						completed: !changedTodo.completed }, unChangedTodo],
+					filter: 'completed',
+				},
+			};
+			const expectation = [{ ...changedTodo,
+				completed: !changedTodo.completed }];
+			const result = filterTodos(ModifiedContext);
+
+			expect(result).toEqual(expectation);
+		});
+	});
+
+	describe('TodosCountCheck', () => {
+		test('hasNoTodos FalseCondition', () => {
+			const result = hasNoTodos(context);
+
+			expect(result).toEqual(false);
+		});
+		test('hasNoTodos TrueCondition', () => {
+			const ModifiedContext = {
+				state: {
+					todos: [],
+				},
+			};
+			const result = hasNoTodos(ModifiedContext);
+
+			expect(result).toEqual(true);
+		});
+	});
+	test('editing the Todo', () => {
+		const data = 'Hi';
+		const result = editTodo(
+			existingTodos, changedTodo, data
+		);
+		const expectation = [{ ...changedTodo,
+			text: data }, unChangedTodo];
+
+		expect(result).toEqual(expectation);
 	});
 });
