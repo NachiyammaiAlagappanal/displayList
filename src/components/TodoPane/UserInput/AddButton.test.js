a435c042df18bb77f2de975ebf1d56e095dd6fc5
NACHIYAMMAI <nachiyammai.al@gmail.com>
2022-04-07 19:06:57 +0530

    AddButton test completed.

---
+++ src/components/TodoPane/UserInput/AddButton.test.js
@@ -0,0 +1,25 @@
+import { render, fireEvent } from '@testing-library/react';
+import AddButton from './AddButton';
+
+describe('Add the text to the Todos', () => {
+	const context = {
+		actions: {
+			addTodo: jest.fn(),
+		},
+		state: Symbol('text'),
+	};
+
+	test('Add the text if text available', () => {
+		const component = render(AddButton(context)).getByRole('AddButton');
+
+		expect(component).toHaveTextContent('+');
+		expect(component).toBeInTheDocument();
+	});
+
+	test('check the accessibility of the clickEvent', () => {
+		const component = render(AddButton(context)).getByRole('AddButton');
+
+		fireEvent.click(component);
+		expect(context.actions.addTodo).toHaveBeenCalledWith();
+	});
+});
