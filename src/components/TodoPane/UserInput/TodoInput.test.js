509a65306212dc81f5803db3d023d078167328de
NACHIYAMMAI <nachiyammai.al@gmail.com>
2022-04-09 11:57:56 +0530

    refactored the test , todoInput

--- src/components/TodoPane/UserInput/TodoInput.test.js
+++ src/components/TodoPane/UserInput/TodoInput.test.js
@@ -2,85 +2,77 @@ import { random } from '@laufire/utils';
 import { render, fireEvent } from '@testing-library/react';
 import inputTextBox from './TodoInput';
 
+const actions = {
+	updateInput: jest.fn(),
+	addTodo: jest.fn(),
+	editTodo: jest.fn(),
+};
+
 describe('TodoInput', () => {
 	const context = {
 		state: {
 			input: '',
 		},
-		actions: {
-			updateInput: jest.fn(),
-			addTodo: jest.fn(),
-			editTodo: jest.fn(),
-		},
+		actions: actions,
 	};
 
 	test('To check the presence of textBox', () => {
-		const result = render(inputTextBox(context)).getByRole('textBox');
+		const textBox = render(inputTextBox(context)).getByRole('textBox');
 
-		expect(result).toBeInTheDocument();
+		expect(textBox).toBeInTheDocument();
 	});
-	test('change the input in state by value', () => {
+
+	test('changes are reflected to state by value', () => {
 		const length = 8;
 		const value = random.rndString(length);
-		const result = render(inputTextBox(context)).getByRole('textBox');
+		const textBox = render(inputTextBox(context)).getByRole('textBox');
 
-		fireEvent.change(result, { target: { value }});
+		fireEvent.change(textBox, { target: { value }});
 		expect(context.actions.updateInput).toHaveBeenCalledWith(value);
 	});
+
 	test('ActionKeys are not match with the given case', () => {
 		const code = random.rndString(1);
-		const result = render(inputTextBox(context)).getByRole('textBox');
+		const textBox = render(inputTextBox(context)).getByRole('textBox');
 
-		fireEvent.keyUp(result, { code });
+		fireEvent.keyUp(textBox, { code });
 		expect(context.actions.addTodo).not.toHaveBeenCalledWith(context);
 		expect(context.actions.editTodo).not.toHaveBeenCalledWith(context);
 		expect(context.actions.updateInput).not.toHaveBeenCalledWith(context);
 	});
 });
+
 describe('ActionKey usage', () => {
+	const testEnterKey = (editing, action) => {
+		const context = {
+			state: {
+				editing,
+			},
+			actions: actions,
+		};
+
+		const textBox = render(inputTextBox(context)).getByRole('textBox');
+
+		fireEvent.keyUp(textBox, { code: 'Enter' });
+		expect(context.actions[action]).toHaveBeenCalledWith(context);
+	};
+
 	test('Enter key to Add the todo', () => {
-		const context = {
-			state: {
-				editing: false,
-			},
-			actions: {
-				addTodo: jest.fn(),
-			},
-		};
-		const code = 'Enter';
-		const result = render(inputTextBox(context)).getByRole('textBox');
-
-		fireEvent.keyUp(result, { code });
-		expect(context.actions.addTodo).toHaveBeenCalledWith(context);
+		testEnterKey(null, 'addTodo');
 	});
+
 	test('Enter key to edit the todo', () => {
-		const context = {
-			state: {
-				editing: true,
-			},
-			actions: {
-				editTodo: jest.fn(),
-			},
-		};
-		const code = 'Enter';
-		const result = render(inputTextBox(context)).getByRole('textBox');
-
-		fireEvent.keyUp(result, { code });
-		expect(context.actions.editTodo).toHaveBeenCalledWith(context);
+		testEnterKey({}, 'editTodo');
 	});
+
 	test('Escape key to set input as empty', () => {
 		const context = {
-			state: {
-				editing: true,
-			},
-			actions: {
-				updateInput: jest.fn(),
-			},
+			actions: actions,
+			state: {},
 		};
-		const code = 'Escape';
-		const result = render(inputTextBox(context)).getByRole('textBox');
+		const textBox = render(inputTextBox(context)).getByRole('textBox');
 
-		fireEvent.keyUp(result, { code });
+		fireEvent.keyUp(textBox, { code: 'Escape' });
 		expect(context.actions.updateInput).toHaveBeenCalledWith('');
 	});
 });
