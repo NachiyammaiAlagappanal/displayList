2d58b6d610d89edb12bde4cc91c959d401fc1bfd
abdul-abstractit <ars@abstractit.in>
2022-10-14 11:16:49 +0530

    Introduced update.

--- src/services/TodoBackend.js
+++ src/services/TodoBackend.js
@@ -1,19 +1,31 @@
 import { omit } from '@laufire/utils/collection';
 import axios from 'axios';
 
+const url = 'http://localhost';
+const port = 1235;
+const headerConfig = {
+	headers: {
+		'Content-Type': 'application/json',
+	},
+};
+
 const TodoBackend = {
 	create: async (todo) => {
 		const res = await axios.post(
-			'http://localhost:1234/todos', todo,
-			{
-				headers: {
-					'Content-Type': 'application/json',
-				},
-			}
+			`${ url }:${ port }/todos`, todo, headerConfig,
 		);
-		const createdData = omit(res.data.data, ['updatedAt', 'createdAt']);
+		const { data: createdData } = res.data;
 
-		return createdData;
+		return omit(createdData, ['updatedAt', 'createdAt']);
+	},
+
+	update: async (id, todo) => {
+		const res = await axios.put(
+			`${ url }:${ port }/todos/${ id }`, todo, headerConfig,
+		);
+		const { data: updatedData } = res.data;
+
+		return omit(updatedData, ['updatedAt', 'createdAt']);
 	},
 };
 
