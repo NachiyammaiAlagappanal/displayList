98b70020c6afe1823e387ab5de4d01ac821e4342
abdul-abstractit <ars@abstractit.in>
2022-10-26 10:30:48 +0530

    Refactored.

--- src/services/Resource.js
+++ src/services/Resource.js
@@ -1,68 +1,53 @@
 import { map, omit } from '@laufire/utils/collection';
 
-const create = async ({ client, name, data }) => {
-	const res = await client({
-		name: name,
-		action: 'create',
-		data: data,
-	});
-	const { data: createdData } = res.data;
+const operations = {
+	create: async ({ client, name, action, data }) => {
+		const res = await client({ name, action, data });
+		const { data: createdData } = res.data;
 
-	return omit(createdData, ['updatedAt', 'createdAt']);
+		return omit(createdData, ['updatedAt', 'createdAt']);
+	},
+
+	getAll: async ({ client, name, action }) => {
+		const res = await client({ name, action });
+		const { data: collections } = res.data;
+		const requiredCollections = map(collections, (collection) =>
+			omit(collection, ['updatedAt', 'createdAt']));
+
+		return requiredCollections;
+	},
+
+	get: async ({ client, name, action, id }) => {
+		const res = await client({ name, action, id });
+		const { data: fetchedData } = res.data;
+
+		return omit(fetchedData, ['updatedAt', 'createdAt']);
+	},
+
+	update: async ({ client, name, action, id, data }) => {
+		const res = await client({ name, action, data, id });
+		const { data: updatedData } = res.data;
+
+		return omit(updatedData, ['updatedAt', 'createdAt']);
+	},
+
+	remove: async ({ client, name, action, id }) => {
+		const res = await client({ name, action, id });
+		const { status } = res.data;
+
+		return status;
+	},
 };
 
-const getAll = async ({ client, name }) => {
-	const res = await client({
-		name: name,
-		action: 'getAll',
-	});
-	const { data: collections } = res.data;
-	const requiredCollections = map(collections, (collection) =>
-		omit(collection, ['updatedAt', 'createdAt']));
-
-	return requiredCollections;
-};
-
-const get = async ({ client, name, id }) => {
-	const res = await client({
-		name: name,
-		action: 'get',
-		id: id,
-	});
-	const { data: fetchedData } = res.data;
-
-	return omit(fetchedData, ['updatedAt', 'createdAt']);
-};
-
-const update = async ({ client, name, id, data }) => {
-	const res = await client({
-		name: name,
-		action: 'update',
-		data: data,
-		id: id,
-	});
-	const { data: updatedData } = res.data;
-
-	return omit(updatedData, ['updatedAt', 'createdAt']);
-};
-
-const remove = async ({ client, name, id }) => {
-	const res = await client({
-		name: name,
-		action: 'remove',
-		id: id,
-	});
-	const { status } = res.data;
-
-	return status;
-};
+const genClientFn = ({ action, client, name }) =>
+	(props) => operations[action]({ client, name, action, ...props });
 
 const resource = ({ name, client }) => ({
-	create: ({ data }) => create({ client, name, data }),
-	getAll: () => getAll({ client, name }),
-	get: ({ id }) => get({ client, name, id }),
-	update: ({ id, data }) => update({ client, name, id, data }),
-	remove: ({ id }) => remove({ client, name, id }),
+	create: genClientFn({ client: client, name: name, action: 'create' }),
+	getAll: genClientFn({ client: client, name: name, action: 'getAll' }),
+	get: genClientFn({ client: client, name: name, action: 'get' }),
+	update: genClientFn({ client: client, name: name, action: 'update' }),
+	remove: genClientFn({ client: client, name: name, action: 'remove' }),
 });
 
 export default resource;
